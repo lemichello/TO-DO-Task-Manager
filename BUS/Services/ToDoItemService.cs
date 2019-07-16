87b1cc111d24307a8b711f801aab4fb6d8dbf7e9
Lemich Maksym <lemichello.m@gmail.com>
2019-07-16 10:53:29 +0300

    fix(BUS): edit ToDoItemService

--- BUS/Services/ToDoItemService.cs
+++ BUS/Services/ToDoItemService.cs
@@ -10,32 +10,27 @@ namespace BUS.Services
     public class ToDoItemService
     {
         private readonly IRepository<ToDoItem> _repository;
-        private readonly int _userId;
+        private readonly int                   _userId;
 
         public ToDoItemService(int userId)
         {
             _repository = new ToDoItemsRepository();
-            _userId = userId;
+            _userId     = userId;
         }
 
         public void Add(ToDoItemModel item)
         {
             var addingItem = new ToDoItem
             {
-                Header   = item.Header,
-                Notes    = item.Notes,
-                Date     = item.Date,
-                Deadline = item.Deadline,
+                Header       = item.Header,
+                Notes        = item.Notes,
+                Date         = item.Date,
+                Deadline     = item.Deadline,
                 CompleteDate = item.CompleteDay,
-                UserId = _userId
+                UserId       = _userId
             };
 
-            if (_repository.Add(addingItem))
-            {
-                item.Id = addingItem.Id;
-            }
-            else
-                item.Id = -1;
+            item.Id = _repository.Add(addingItem) ? addingItem.Id : -1;
         }
 
         public bool Remove(ToDoItemModel item)
@@ -49,26 +44,29 @@ namespace BUS.Services
         {
             var foundItem = _repository.Get().First(i => i.Id == item.Id);
 
-            foundItem.Header = item.Header;
-            foundItem.Notes = item.Notes;
-            foundItem.Date = item.Date;
-            foundItem.Deadline = item.Deadline;
+            foundItem.Header       = item.Header;
+            foundItem.Notes        = item.Notes;
+            foundItem.Date         = item.Date;
+            foundItem.Deadline     = item.Deadline;
             foundItem.CompleteDate = item.CompleteDay;
-            
+
             _repository.SaveChanges();
         }
-        
+
         public IEnumerable<ToDoItemModel> Get(Func<ToDoItemModel, bool> predicate)
         {
-            return _repository.Get().Select(i => new ToDoItemModel
-            {
-                Id       = i.Id,
-                Header   = i.Header,
-                Notes    = i.Notes,
-                Date     = i.Date,
-                Deadline = i.Deadline,
-                CompleteDay = i.CompleteDate
-            }).Where(predicate);
+            return _repository.Get()
+                .Where(i => i.UserId == _userId)
+                .Select(i => new ToDoItemModel
+                {
+                    Id          = i.Id,
+                    Header      = i.Header,
+                    Notes       = i.Notes,
+                    Date        = i.Date,
+                    Deadline    = i.Deadline,
+                    CompleteDay = i.CompleteDate
+                })
+                .Where(predicate);
         }
     }
 }
