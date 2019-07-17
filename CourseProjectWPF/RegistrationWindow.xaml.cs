fbcf6cd9d5da73b66fc5ce8a886bfae194220d21
Lemich Maksym <lemichello.m@gmail.com>
2019-07-17 19:37:37 +0300

    feat(CourseProjectWPF): add Registration window

---
+++ CourseProjectWPF/RegistrationWindow.xaml.cs
@@ -0,0 +1,40 @@
+using System.Windows;
+using BUS.Services;
+
+namespace CourseProjectWPF
+{
+    public partial class RegistrationWindow : Window
+    {
+        private readonly UserService _service;
+        
+        public RegistrationWindow(UserService service)
+        {
+            InitializeComponent();
+
+            _service = service;
+        }
+
+        private void RegisterButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(LoginTextBox.Text) ||
+                string.IsNullOrWhiteSpace(PasswordBox.Password) ||
+                string.IsNullOrWhiteSpace(RepeatPasswordBox.Password))
+            {
+                MessageBox.Show("You need to fill all fields.");
+                return;
+            }
+
+            if (PasswordBox.Password != RepeatPasswordBox.Password)
+            {
+                MessageBox.Show("Passwords don\'t match");
+                return;
+            }
+
+            if(_service.Add(LoginTextBox.Text, PasswordBox.Password))
+            {
+                MessageBox.Show("Successfully registered.");
+                Close();
+            }
+        }
+    }
+}
